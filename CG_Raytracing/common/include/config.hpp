065c4937d60dbdc4824c10f943a2cfb3ec2b07dd
Filippo831 <filippoarduini83@gmail.com>
2026-05-09 13:31:44 +0200

    feat: cmake fixes, camera constructor

--- CG_Raytracing/common/include/config.hpp
+++ CG_Raytracing/common/include/config.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
+#include <cstdint>
 namespace Config {
 inline constexpr double ASPECT_RATIO = 16.0 / 9.0;
-inline constexpr int IMAGE_WIDTH = 400;
-inline constexpr int IMAGE_HEIGHT =
-    static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
+inline constexpr uint32_t IMAGE_WIDTH = 400;
+inline constexpr uint32_t IMAGE_HEIGHT =
+    static_cast<uint32_t>(IMAGE_WIDTH / ASPECT_RATIO);
 
-inline constexpr double FOCAL_LENGTH = 50;
+inline constexpr uint32_t FOCAL_LENGTH = 50; // focal length in mm
+inline constexpr uint32_t SENSOR_SIZE_WIDTH =
+    36; // sensor size in mm (36 is full-frame)
 
-inline constexpr int RAY_PER_PIXEL = 10;
-inline constexpr int RENDER_ITERATION = 10;
+inline constexpr float CAMERA_POSITION[3] = {0.0, 0.0, 0.0};
+inline constexpr float CAMERA_DIRECTION[3] = {1.0, 0.0, 0.0};
+
+inline constexpr uint32_t RAY_PER_PIXEL = 10;
+inline constexpr uint32_t RENDER_ITERATION = 10;
 } // namespace Config
--- CG_Raytracing/src/scene/include/camera.hpp
+++ CG_Raytracing/src/scene/include/camera.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
+#include "config.hpp"
+#include <cstdint>
 #include <iostream>
 
 class Camera {
   public:
-    Camera() {
-        std::println(std::cout, "Camera class constructor");
+    Camera(uint32_t _sensor_size_width = Config::SENSOR_SIZE_WIDTH,
+           uint32_t _focal_length = Config::FOCAL_LENGTH,
+           uint32_t _width = Config::IMAGE_WIDTH,
+           uint32_t _height = Config::IMAGE_HEIGHT,
+           const float *_position = Config::CAMERA_POSITION,
+           const float *_direction = Config::CAMERA_DIRECTION)
+        : m_sensor_size_width(_sensor_size_width),
+          m_focal_length(_focal_length), m_width(_width), m_height(_height) {
+        std::copy(_position, _position + 3, m_position);
+        std::copy(_direction, _direction + 3, m_direction);
     }
+    uint32_t m_sensor_size_width;
+    uint32_t m_focal_length;
+    uint32_t m_width;
+    uint32_t m_height;
+    float m_position[3];
+    float m_direction[3];
 };
