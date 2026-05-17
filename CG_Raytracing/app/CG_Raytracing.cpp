7623e4dc5372bd127389ceb0caa5cde8c04ea949
Claudio <claudio.vozza@buonarroti.tn.it>
2026-05-17 10:15:40 +0200

    (MAIN) Print first frame render time

--- CG_Raytracing/app/CG_Raytracing.cpp
+++ CG_Raytracing/app/CG_Raytracing.cpp
@@ -32,6 +32,7 @@
 #include <iostream>
 #include <memory>
 #include <print>
+#include <chrono>
 
 void DebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
                    GLsizei length, const GLchar *message,
@@ -304,7 +305,13 @@ int main() {
 
     world.UpdateTree();
 
+    auto begin = std::chrono::system_clock::now();
+
     my_camera->BurstRays(light, world);
+
+    auto end = std::chrono::system_clock::now();
+    auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+    std::println(std::cout, "Took {} ms", (double)diff / 1e3);
 
 
     tex.CopyFromBuffer(my_camera->m_img_buf.data(), 0, 0, 0, tex.GetWidth(),
